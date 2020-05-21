e37c679789a254d9c15e475b09972eba03379d66
niuhb2 <niuhaibao@gmail.com>
2020-05-21 13:43:56 +0800

    update .gitignore

---
+++ 2020_05_14/2020_05_14.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 开发人员   ：Davis Niu
+# 开发时间   ：5/14/2020  01:50 PM 
+# 文件名称   ：2020_05_14.PY
+
+
+def solution(number):
+    """
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+    Note: If the number is a multiple of both 3 and 5, only count it once.
+    :param number:
+    :return:
+    """
